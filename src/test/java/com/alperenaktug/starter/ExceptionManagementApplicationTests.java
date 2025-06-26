927c49b23adddaf3d682485fd2e25cc24aa233ff
Alperen Aktuğ <aktugalperen9@gmail.com>
2025-06-26 23:37:10 +0300

    junit test

--- src/test/java/com/alperenaktug/starter/ExceptionManagementApplicationTests.java
+++ src/test/java/com/alperenaktug/starter/ExceptionManagementApplicationTests.java
@@ -1,13 +1,41 @@
 package com.alperenaktug.starter;
 
+import com.alperenaktug.dto.DtoEmployee;
+import com.alperenaktug.service.IEmployeeService;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@SpringBootTest(classes = {ExceptionManagementStarter.class})
 class ExceptionManagementApplicationTests {
 
+    @Autowired
+    private IEmployeeService employeeService;
+
+
+    @BeforeEach
+    public void beforeEach(){
+        System.out.println("beforeEach running!!");
+    }
+
     @Test
-    void contextLoads() {
+    public void testFindEmployeeById(){
+      DtoEmployee employee = employeeService.findEmployeeById(4L);
+      assertNotNull(employee);
+      /*
+      if(employee!=null){
+          System.out.println("isim : " + employee.getName());
+      }
+      */
+    }
+
+    @AfterEach
+    public void afterEach(){
+        System.out.println("afterEach running!!");
     }
 
 }
